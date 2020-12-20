714a0b070fc1d9535dc549be6b5fdc2c7e4083c3
wangtq <wangtq@asiainfo.com>
2020-12-20 15:38:16 +0800

    配置化表单（FormCard, 可以监听任意表单内元素的值，可以监听表单元素对应的各个子组件中的emit事件，单独用组件@监听）： 1：Input(输入框) 2：TextInput（文本框） 3：BaseEditor（富文本框) 4：label(render渲染，lable数据转换（待做~~）) 5: SelectDefault(下拉框-静态数据) 6、DictSelect（下拉框-数据字典） 7、SelectService（下拉框-自定义接口） 8、SelectTreeService（下拉框-自定义接口-树结构）
    数据大屏（基本框架，自适应）
    1、自适应架构搭建
    2、大屏-地图展示
    3、大屏-报表展示
    
    组件（Breadcrumb，Charts， DataScreen， Editor， hamburger， pageCard， Progress）

--- src/components/PageCard/index.vue
+++ src/components/PageCard/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="page-card__body">
     <el-card body-style="padding: 0" shadow="never">
-      <div v-if="$slots.header || header" slot="header" style="display: flex;align-items: center;">
+      <div v-if="$slots.header || header" slot="header" style="display: flex;align-items: center">
         <div class="lalal-color" style="width: 10px;height: 10px;margin-right: 5px"/>
-        <slot name="header">
-          {{ header }}
-        </slot>
+        <div class="page-card-header">
+          <div>
+            <slot name="header">
+              {{ header }}
+            </slot>
+          </div>
+          <div>
+            <slot name="rightHeader"></slot>
+          </div>
+        </div>
       </div>
       <div class="height100" style="padding: 15px 15px;min-height: 300px;position: relative">
         <slot />
@@ -41,6 +48,12 @@ export default {
 <style lang="less">
 @import "~@/styles/element-variables.less";
 .page-card__body{
+  .page-card-header{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
   .height100{
     max-height: calc(~'100vh - 200px');
     overflow-y: auto;
--- src/view/report/screen/index.vue
+++ src/view/report/screen/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <PageCard header="数据大屏">
-      <el-button @click="goto()">跳转到大屏</el-button>
+    <PageCard>
+      <div slot="header">数据大屏</div>
+      <div slot="rightHeader"> <el-button @click="goto()">跳转到大屏</el-button></div>
+      <div class="main-screen_body">
+        <DataScreen>
+          <BigScreenContent></BigScreenContent>
+        </DataScreen>
+      </div>
     </PageCard>
   </div>
 </template>
 
 <script>
+  import BigScreenContent from "../bigScreen/bigScreenContent";
   export default {
     name: 'screen',
-    components:{},
+    components:{BigScreenContent},
     data() {
       return {}
     },
@@ -24,4 +31,8 @@
 </script>
 
 <style lang="less">
+  .main-screen_body{
+    width: 1344px;
+    height: 756px;
+  }
 </style>
